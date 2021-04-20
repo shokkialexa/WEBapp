59984d9cf314892547c15dbc5cf3e5724e720409
Alexandra <alexandra-dimitrichenko@yandex.ru>
2021-04-20 18:10:20 +0300

    fix english

--- forms/settings_form.py
+++ forms/settings_form.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, NumberRange
 
 
 class SettingsForm(FlaskForm):
-    theme = SelectField('Fon', validators=[DataRequired()],
+    theme = SelectField('Background', validators=[DataRequired()],
                         choices=['default', 'magic girl', 'Odyssey', 'lonely mountain', 'winter dreams',
                                  'people routine', 'house in the forest'])
     number_of_cards_in_hand = IntegerField('Number of cards in hand', validators=[DataRequired(), NumberRange(max=9,
